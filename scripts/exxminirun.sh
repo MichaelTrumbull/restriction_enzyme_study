d9f3a83957f7d06ac085ebea0e73b19582d69904
MichaelTrumbull <michaeljamestrumbull@gmail.com>
2022-10-05 16:13:36 -0400

    fixed if else

--- scripts/exxminirun.sh
+++ scripts/exxminirun.sh
@@ -6,8 +6,11 @@ nvidia-smi # see what gpu is avail
 echo Which GPU is available? Use integer value. Empty to cancel run
 read gpu_number
 echo Running trainmodel.py with CUDA:$gpu_number
-if ((number >= 0 && number <= 3)); then
+
+if ((number >= 0 && number <= 3))
+then
   #python model/trainmodel.py --flags
+  echo finished
 else
   echo Run cancelled
 fi
