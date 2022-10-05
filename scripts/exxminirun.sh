73625678d55297e90df1bac546418fdc863a7bd2
MichaelTrumbull <michaeljamestrumbull@gmail.com>
2022-10-05 16:11:40 -0400

    if else added

--- scripts/exxminirun.sh
+++ scripts/exxminirun.sh
@@ -3,8 +3,11 @@ conda activate metal_motifs_env
 
 nvidia-smi # see what gpu is avail
 
-echo Which GPU is available? Use integer value
+echo Which GPU is available? Use integer value. Empty to cancel run
 read gpu_number
 echo Running trainmodel.py with CUDA:$gpu_number
-
-#python model/trainmodel.py --flags
+if ((number >= 0 && number <= 3)); then
+  #python model/trainmodel.py --flags
+else
+  echo Run cancelled
+fi
