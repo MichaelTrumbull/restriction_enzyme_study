cf02754c8426abe785ed1d434a6055b6928395c8
MichaelTrumbull <michaeljamestrumbull@gmail.com>
2022-10-05 16:07:31 -0400

    fixed exxxmini run

--- scripts/exxminirun.sh
+++ scripts/exxminirun.sh
@@ -3,7 +3,8 @@ conda activate metal_motifs_env
 
 nvidia-smi # see what gpu is avail
 
-echo Which GPU is available? (enter integer val)
+echo Which GPU is available?
+echo Integer value:
 read gpu_number
 echo Running trainmodel.py with CUDA:$gpu_number
 
