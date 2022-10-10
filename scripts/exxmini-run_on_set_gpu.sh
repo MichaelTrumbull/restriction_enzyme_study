c2dd74c6bc06f0e4e9953643a18780a492c531f6
MichaelTrumbull <michaeljamestrumbull@gmail.com>
2022-10-10 15:27:37 -0400

    modified big run

--- scripts/exxmini-run_on_set_gpu.sh
+++ scripts/exxmini-run_on_set_gpu.sh
@@ -4,6 +4,13 @@ conda activate metal_motifs_env
 export CUDA_VISIBLE_DEVICES=0
 python model/trainmodel.py --batch 6
 python model/trainmodel.py --batch 12
+python model/trainmodel.py --batch 20
 python model/trainmodel.py --batch 6 --hid 1
+python model/trainmodel.py --batch 15 --hid 1
+python model/trainmodel.py --batch 20 --hid 1
+python model/trainmodel.py --batch 30 --hid 1
 python model/trainmodel.py --batch 6 --hid 2
-python model/trainmodel.py --batch 6 --target_path data/Motif_1stHalf_Motif_2ndHalf_numN.pt
+python model/trainmodel.py --batch 15 --hid 2
+python model/trainmodel.py --batch 15 --target_path data/Motif_1stHalf_Motif_2ndHalf_numN.pt
+python model/trainmodel.py --batch 15 --hid 1 --target_path data/Motif_1stHalf_Motif_2ndHalf_numN.pt
+python model/trainmodel.py --batch 15 --hid 2 --target_path data/Motif_1stHalf_Motif_2ndHalf_numN.pt
