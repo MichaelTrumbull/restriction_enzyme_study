398d5f65a864a26cd31053688d65b38298eb1e85
MichaelTrumbull <michaeljamestrumbull@gmail.com>
2022-10-10 15:03:48 -0400

    new loss func for new target data

--- model/trainmodel.py
+++ model/trainmodel.py
@@ -11,7 +11,7 @@ import os
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--epochs', type=int, default=49, help="integer value of number of epochs to run for")
+parser.add_argument('--epochs', type=int, default=20, help="integer value of number of epochs to run for")
 parser.add_argument('--connections', type=int, default=256, help="number of connections between nodes in linear layers")
 parser.add_argument('--hid', type=int, default=0, help="number of hidden linear layers in the network")
 parser.add_argument('--lrval', type=float, default=0.001, help="lrval jump value during training")
@@ -29,6 +29,11 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 hid = args.hid
 con = args.connections
 lrval = args.lrval
+
+if args.target_path == "data/metalation_motifs_onehot_pad.pt": 
+    met_mot = True # This target data (also pseudo one hot encoded) is not spaced according to the original loss func i made. A new one with the correct positioning is needed
+else:
+    met_mot = False
 
 run_name = datetime.now().strftime("%m-%d-%H-%M")
 savepath = "runs/" + run_name
@@ -61,6 +66,7 @@ with open(savepath + "/setup.log", "w") as f:
     f.write('--target_path:' + str(args.target_path) + "\n")
     f.write("train_x.size():" + str(train_x.size()) + "\n")
     f.write("train_y.size():" + str(train_y.size()) + "\n")
+    f.write("met_mot:" + str(met_mot) + "\n")
     f.write(str(net) + "\n")
 
 #split_crossentropy = networks.split_crossentropy() # test if I can import this from networks to clean up training script IF THIS FAILS JUST COPY AND PASTE FROM NETWORKS BACK IN
@@ -71,7 +77,7 @@ def save_losses(temp_hold_losses):
         f.write(str(temp_hold_losses))
 
 crossentropy = nn.CrossEntropyLoss()
-def split_crossentropy(a, target):
+def split_crossentropy_motif1st2ndhalf(a, target):
     bases = a[:,0:105-9] # len=96 = 24 * 4base
     target_bases = target[:,0:105-9]
     spaces = a[:,105-9:105] # 9 digit one hot representing number of spaces
@@ -83,6 +89,15 @@ def split_crossentropy(a, target):
         hold.append( temp ) #cat the soft max of a set of 4 onto hold
     hold.append( crossentropy(spaces, target_spaces).item() ) #cat the soft max of the number of spaces
     return torch.tensor( sum(hold)/25 , requires_grad=True).to(device=device) # , dtype=torch.float )
+def split_crossentropy_met_mot(a, target): # this should be used if target data is from "data/metalation_motifs_onehot_pad.pt"
+    bases = a[:,:] # All are bases in this 136 bit encoding. 
+    target_bases = target[:,:] # All are bases. There are no 'spaces' accounted for at the end
+    hold = []
+    hold.append( crossentropy(bases[:,0:4], target_bases[:,0:4]).item() )# first base
+    for i in range(int(len(bases[0])/4) - 1):
+        temp =  crossentropy(bases[:,(i+1)*4:(i+2)*4], target_bases[:,(i+1)*4:(i+2)*4]).item()
+        hold.append( temp ) #cat the soft max of a set of 4 onto hold
+    return torch.tensor( sum(hold)/34 , requires_grad=True).to(device=device) # , dtype=torch.float )
 
 optimizer = optim.Adam(net.parameters(), lr=lrval)
 BATCH_SIZE = args.batch
@@ -101,7 +116,10 @@ for epoch in range(EPOCHS):
 
         net.zero_grad()
         outputs = net(batch_x)
-        loss = split_crossentropy(outputs, batch_y)
+        if met_mot: 
+            split_crossentropy_met_mot(outputs, batch_y)
+        else:
+            loss = split_crossentropy_motif1st2ndhalf(outputs, batch_y)
         loss.backward()
         optimizer.step()
         hold_losses.append(loss.item()) # was originally outside batch loop...
