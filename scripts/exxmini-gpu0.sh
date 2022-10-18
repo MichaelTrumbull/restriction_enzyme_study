6cec97ac04ad8a74677cf4edcf376aee459ab0fd
MichaelTrumbull <38739363+MichaelTrumbull@users.noreply.github.com>
2022-10-18 10:25:34 -0400

    testing github dev

--- scripts/exxmini-gpu0.sh
+++ scripts/exxmini-gpu0.sh
@@ -19,3 +19,5 @@ CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 3 --epochs 19 --hid 1 
 
 CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 15 --epochs 19 --hid 0 --input_path data/input_esm2_3B_layer33_1dseq_padlast_tokenrep.pt --target_path data/target_motif1stHalf2ndHalf_padmiddle_numN.pt
 CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 3 --epochs 19 --hid 1 --input_path data/input_esm2_3B_layer33_1dseq_padlast_tokenrep.pt --target_path data/target_motif1stHalf2ndHalf_padmiddle_numN.pt
+
+# testing github.dev
