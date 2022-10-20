cb52d672dc1135e1738296be04d72c92c0d51706
MichaelTrumbull <michaeljamestrumbull@gmail.com>
2022-10-20 11:20:40 -0400

    new long run

--- scripts/exxmini-gpu0.sh
+++ scripts/exxmini-gpu0.sh
@@ -2,13 +2,13 @@ source lib/conda/bin/activate
 conda activate metal_motifs_env
 
 echo 1
-CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 15 --epochs 19 --hid 0 --input_path data/input_esm2_3B_layer33_1dseq_padlast_sequencerep.pt --target_path data/target_Methylation_Motif_padmiddle.pt
+CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 15 --epochs 99 --hid 0 --input_path data/input_esm2_3B_layer33_1dseq_padlast_sequencerep.pt --target_path data/target_Methylation_Motif_padmiddle.pt
 echo 2
-CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 3 --epochs 19 --hid 1 --input_path data/input_esm2_3B_layer33_1dseq_padlast_sequencerep.pt --target_path data/target_Methylation_Motif_padmiddle.pt
+CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 3 --epochs 99 --hid 1 --input_path data/input_esm2_3B_layer33_1dseq_padlast_sequencerep.pt --target_path data/target_Methylation_Motif_padmiddle.pt
 
 
 echo 3 - reduced batch size
-CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 1 --epochs 19 --hid 0 --input_path data/input_esm2_3B_layer33_1dseq_padlast_tokenrep.pt --target_path data/target_Methylation_Motif_padmiddle.pt
+CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 1 --epochs 99 --hid 0 --input_path data/input_esm2_3B_layer33_1dseq_padlast_tokenrep.pt --target_path data/target_Methylation_Motif_padmiddle.pt
 echo not running 4 - batch cant go smaller
 #CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 3 --epochs 19 --hid 1 --input_path data/input_esm2_3B_layer33_1dseq_padlast_tokenrep.pt --target_path data/target_Methylation_Motif_padmiddle.pt
 
@@ -16,27 +16,44 @@ echo not running 4 - batch cant go smaller
 
 
 echo 5
-CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 15 --epochs 19 --hid 0 --input_path data/input_esm2_3B_layer33_1dseq_padlast_sequencerep.pt --target_path data/target_Methylation_Motif_padlast.pt
+CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 15 --epochs 99 --hid 0 --input_path data/input_esm2_3B_layer33_1dseq_padlast_sequencerep.pt --target_path data/target_Methylation_Motif_padlast.pt
 echo 6
-CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 3 --epochs 19 --hid 1 --input_path data/input_esm2_3B_layer33_1dseq_padlast_sequencerep.pt --target_path data/target_Methylation_Motif_padlast.pt
+CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 3 --epochs 99 --hid 1 --input_path data/input_esm2_3B_layer33_1dseq_padlast_sequencerep.pt --target_path data/target_Methylation_Motif_padlast.pt
 
 
 echo 7 - reduced batch size
-CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 1 --epochs 19 --hid 0 --input_path data/input_esm2_3B_layer33_1dseq_padlast_tokenrep.pt --target_path data/target_Methylation_Motif_padlast.pt
+CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 1 --epochs 99 --hid 0 --input_path data/input_esm2_3B_layer33_1dseq_padlast_tokenrep.pt --target_path data/target_Methylation_Motif_padlast.pt
 echo not running 8 - batch cant go smaller
 #CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 3 --epochs 19 --hid 1 --input_path data/input_esm2_3B_layer33_1dseq_padlast_tokenrep.pt --target_path data/target_Methylation_Motif_padlast.pt
 
 
 echo 9
-CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 15 --epochs 19 --hid 0 --input_path data/input_esm2_3B_layer33_1dseq_padlast_sequencerep.pt --target_path data/target_motif1stHalf2ndHalf_padmiddle_numN.pt
+CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 15 --epochs 99 --hid 0 --input_path data/input_esm2_3B_layer33_1dseq_padlast_sequencerep.pt --target_path data/target_motif1stHalf2ndHalf_padmiddle_numN.pt
 echo 10
-CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 3 --epochs 19 --hid 1 --input_path data/input_esm2_3B_layer33_1dseq_padlast_sequencerep.pt --target_path data/target_motif1stHalf2ndHalf_padmiddle_numN.pt
+CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 3 --epochs 99 --hid 1 --input_path data/input_esm2_3B_layer33_1dseq_padlast_sequencerep.pt --target_path data/target_motif1stHalf2ndHalf_padmiddle_numN.pt
 
 
 echo 11 - reduce batch size
-CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 1 --epochs 19 --hid 0 --input_path data/input_esm2_3B_layer33_1dseq_padlast_tokenrep.pt --target_path data/target_motif1stHalf2ndHalf_padmiddle_numN.pt
+CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 1 --epochs 99 --hid 0 --input_path data/input_esm2_3B_layer33_1dseq_padlast_tokenrep.pt --target_path data/target_motif1stHalf2ndHalf_padmiddle_numN.pt
 echo not running 12 - batch cant go smaller
 #CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 3 --epochs 19 --hid 1 --input_path data/input_esm2_3B_layer33_1dseq_padlast_tokenrep.pt --target_path data/target_motif1stHalf2ndHalf_padmiddle_numN.pt
+
+echo more layers
+echo hid 2
+CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 3 --epochs 99 --hid 2 --input_path data/input_esm2_3B_layer33_1dseq_padlast_sequencerep.pt --target_path data/target_Methylation_Motif_padmiddle.pt
+echo hid 3
+CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 3 --epochs 99 --hid 3 --input_path data/input_esm2_3B_layer33_1dseq_padlast_sequencerep.pt --target_path data/target_Methylation_Motif_padmiddle.pt
+echo hid 4
+CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 3 --epochs 99 --hid 4 --input_path data/input_esm2_3B_layer33_1dseq_padlast_sequencerep.pt --target_path data/target_Methylation_Motif_padmiddle.pt
+echo more layers run again but now with less connections
+echo hid 1
+CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 3 --epochs 99 --hid 1 --connections 128 --input_path data/input_esm2_3B_layer33_1dseq_padlast_sequencerep.pt --target_path data/target_Methylation_Motif_padmiddle.pt
+echo hid 2
+CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 3 --epochs 99 --hid 2 --connections 128 --input_path data/input_esm2_3B_layer33_1dseq_padlast_sequencerep.pt --target_path data/target_Methylation_Motif_padmiddle.pt
+echo hid 3
+CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 3 --epochs 99 --hid 3 --connections 128 --input_path data/input_esm2_3B_layer33_1dseq_padlast_sequencerep.pt --target_path data/target_Methylation_Motif_padmiddle.pt
+echo hid 4
+CUDA_VISIBLE_DEVICES=0 python model/trainmodel.py --batch 3 --epochs 99 --hid 4 --connections 128 --input_path data/input_esm2_3B_layer33_1dseq_padlast_sequencerep.pt --target_path data/target_Methylation_Motif_padmiddle.pt
 
 
 # testing github.dev
