0412f849ddbc9b5483a90fa65139b4871f96d855
MichaelTrumbull <michaeljamestrumbull@gmail.com>
2022-11-03 13:32:03 -0400

    print statements

--- model/test_cuda_error.py
+++ model/test_cuda_error.py
@@ -6,11 +6,14 @@ class Net_Linear(nn.Module):
     def __init__(self, input_size, output_size):
         super().__init__()
 
+        print('Start of init')
+
         self.i = input_size
         self.o = output_size
 
         self.fc11 = nn.Linear(self.i, self.o)
-
+        
+        print('End of init')
     def forward(self, x):
         x = F.relu(self.fc11(x))
         return x
@@ -18,3 +21,4 @@ class Net_Linear(nn.Module):
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 net = Net_Linear( 7000, 300).to(device=device)
+print('Finished')
