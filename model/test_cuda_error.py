e7c79801438623b7dff7688503ac08092c5a86ea
MichaelTrumbull <michaeljamestrumbull@gmail.com>
2022-11-03 13:30:42 -0400

    shrunk test code

--- model/test_cuda_error.py
+++ model/test_cuda_error.py
@@ -2,29 +2,19 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-print('after imports')
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
-
 class Net_Linear(nn.Module):
-    def __init__(self, input_size=10, output_size=10, hidden_layers=0, connections_between_layers=256):
+    def __init__(self, input_size, output_size):
         super().__init__()
 
-        self.i = input_size #len(train_x[0])
-        self.o = output_size #len(train_y[0])
-        self.hid = hidden_layers
-        self.con = connections_between_layers
+        self.i = input_size
+        self.o = output_size
 
         self.fc11 = nn.Linear(self.i, self.o)
 
-        print('ending init for network')
-
     def forward(self, x):
-        print('starting forward')
         x = F.relu(self.fc11(x))
-        print('running if stateament ')
         return x
 
-net = Net_Linear( 7000, 300, 0, 256).to(device=device)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-print('done')
+net = Net_Linear( 7000, 300).to(device=device)
