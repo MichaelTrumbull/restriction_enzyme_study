c3da18c9d617d6a123c1df86f4cf7b0874cfd512
MichaelTrumbull <michaeljamestrumbull@gmail.com>
2022-11-14 16:11:54 -0500

    rel path

--- scripts/install_conda_env.sh
+++ scripts/install_conda_env.sh
@@ -1,7 +1,7 @@
 # install miniconda into local \lib. 
 # run using sudo command ?
 # need to mkdir runs lib data tmp
-wget -P /tmp "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+wget -P tmp "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
 bash tmp/Miniconda3-latest-Linux-x86_64.sh -b -p lib/conda
 rm tmp/Miniconda3-latest-Linux-x86_64.sh
 
